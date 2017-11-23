deaa3cbed0ead16aed29ae182f852ce209a0996f
xinkong <2624384463@qq.com>
2017-11-23 13:32:11 +0800

    Initial commit

---
+++ imagesel/src/main/java/com/grass/imagesel/TestMaven.java
@@ -0,0 +1,14 @@
+package com.grass.imagesel;
+
+/**
+ * Created by huchao on 2017/11/23.
+ */
+
+public class TestMaven {
+
+    public static String getTest(){
+        return "来自测试Maven打包";
+    }
+
+
+}
